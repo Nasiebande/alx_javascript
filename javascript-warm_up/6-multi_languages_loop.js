b6b0129d2472445d4e12b42bbdc23126d505fa20
Nasiebande <nikitaesther401@gmail.com>
2023-09-04 03:17:46 -0700

    task 2

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Define an array of strings
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Use a for loop to print each line
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
