ea1aaf20803f66e9d9a2e3126b657d9f8d395192
Nasiebande <nikitaesther401@gmail.com>
2023-09-04 03:20:20 -0700

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,9 @@
 
 // Define an array of strings
 const lines = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 // Use a for loop to print each line
