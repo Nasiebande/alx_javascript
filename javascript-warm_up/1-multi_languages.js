fdecae403c28a74c9015af2997f5271412cc5b89
Nasiebande <nikitaesther401@gmail.com>
2023-09-04 03:08:49 -0700

    task 1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+// First line: 'C is fun'
+console.log("C is fun");
+
+// Second line: 'Python is cool'
+console.log("Python is cool");
+
+// Third line: 'JavaScript is amazing'
+console.log("JavaScript is amazing");
