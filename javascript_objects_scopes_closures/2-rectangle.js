350e57015af7fef7a1e854f7919b4810adebb3fe
Nasiebande <nikitaesther401@gmail.com>
2023-09-05 05:09:48 -0700

    task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is 0 or not a positive integer
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+}
+
+module.exports = Rectangle;
