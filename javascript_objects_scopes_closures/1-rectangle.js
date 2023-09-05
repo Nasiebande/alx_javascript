199286d202c60a9035926a891c291bde45dac277
Nasiebande <nikitaesther401@gmail.com>
2023-09-05 05:01:31 -0700

    define a rectangle

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -3,6 +3,10 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+      // If w or h is not provided or negative, set them to undefined
+      this.width = undefined;
+      this.height = undefined;
     }
   }
 }
