9d4bebae679c59ec9e30eb49dfa0ea9ce538be67
Nasiebande <nikitaesther401@gmail.com>
2023-09-05 05:07:10 -0700

    correction

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,9 @@
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
+    if (w !== undefined && h !== undefined && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      // If w or h is not provided or negative, set them to undefined
       this.width = undefined;
       this.height = undefined;
     }
