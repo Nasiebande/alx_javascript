8a1d039937944f1b83db889bbf9e4a8164feca3d
Nasiebande <nikitaesther401@gmail.com>
2023-09-07 12:16:57 -0700

    Task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-
+/*  Rectagnle class with empty object*/
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object
-      return {};
+    constructor(w, h) {
+        if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+            this.width = w;
+            this.height = h;
+        }
     }
-  }
 }
-
 module.exports = Rectangle;
