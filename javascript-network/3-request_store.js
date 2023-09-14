32ed488109819415e1a5bd11d25428971f3fa6b5
Nasiebande <nikitaesther401@gmail.com>
2023-09-14 05:37:29 -0700

    Correction

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,25 +1,20 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 
-// Check if the user provided a URL and a file path as arguments
 const url = process.argv[2];
-const filePath = process.argv[3];
+const fileName = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Please provide a URL and a file path as arguments.');
-  process.exit(1);
-}
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    }
+    const data = body;
 
-// Perform a GET request to the specified URL
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  // Write the response body to the specified file
-  fs.writeFileSync(filePath, body, 'utf-8');
-  console.log(`Data from ${url} has been saved to ${filePath}.`);
+    fs.writeFile(fileName, data, 'utf-8', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
